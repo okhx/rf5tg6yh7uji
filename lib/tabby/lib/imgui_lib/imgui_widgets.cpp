8c779d5f21086e0543c7cdf39934fb38ec439d1f
okhx <swiftandchipsmfc@gmail.com>
2026-07-14 16:46:15 +0500

    Initial commit9

--- lib/tabby/lib/imgui_lib/imgui_widgets.cpp
+++ lib/tabby/lib/imgui_lib/imgui_widgets.cpp
@@ -6342,7 +6342,7 @@ bool ImGui::InputTextEx(const char* label, const char* hint, char* buf,
                 apply_new_text = "";
                 apply_new_text_length = 0;
                 value_changed = true;
-                IMSTB_TEXTEDIT_CHARTYPE empty_string;
+                IMSTB_TEXTEDIT_CHARTYPE empty_string{};
                 stb_textedit_replace(state, state->Stb, &empty_string, 0);
             } else if (strcmp(buf, state->TextToRevertTo.Data) != 0) {
                 apply_new_text = state->TextToRevertTo.Data;
